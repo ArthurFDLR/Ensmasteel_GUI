29e1dde4717042c6366061641fa5e2de50f49a4f
Arthur FINDELAIR <arthfind@gmail.com>
2020-03-31 17:48:08 +0200

    Finish bot recorder

--- VirtualComPort/Bot_Simulator.py
+++ VirtualComPort/Bot_Simulator.py
@@ -3,7 +3,8 @@ import time
 import csv
 
 TIME0 = time.time_ns()
-Time_ms = lambda : (int) (time.time_ns()-TIME0) * 1e-6
+Time_ms = lambda : int((time.time_ns()-TIME0) * 1e-6)
+Time_ns = lambda : time.time_ns()-TIME0
 timeLast_ms = Time_ms()
 
 Ser_Recorded = serial.Serial(
@@ -22,7 +23,8 @@ while not recording:
     line = ""
     if(Ser_Recorded.inWaiting() > 0):
         line = Ser_Recorded.readline()
-        recording = (line == b'received ID 2\r\n') # trigger signal
+        #print(line)
+        recording = ((line == b'received ID 2\r\n') or (line == b'@Tirette|0\n')) # trigger signal
     if(Time_ms() - timeLast_ms > 8e2):
         print(".", end = '', flush=True)
         timeLast_ms = Time_ms()
@@ -30,22 +32,25 @@ while not recording:
 ## Record signal
 print("\n\nStart recording :\n")
 TIME0 = time.time_ns()
-timeLast_ms = Time_ms()
+timeLast_ns = Time_ns()
 
-while True:
-    try:
-        if(Time_ms() - timeLast_ms > 8e2):
-            print(".")
-            timeLast_ms = Time_ms()
-        '''
-        ser_bytes = ser.readline()
-        decoded_bytes = float(ser_bytes[0:len(ser_bytes)-2].decode("utf-8"))
-        print(decoded_bytes)
-        with open("test_data.csv","a") as f:
-            writer = csv.writer(f,delimiter=",")
-            writer.writerow([time.time(),decoded_bytes])
-        '''
-    except:
-        print("Keyboard Interrupt")
-        Ser_Recorded.close()
-        break
+with open("Bot_Telemetry.csv", "w", newline='') as f:
+    writer = csv.writer(f,delimiter=";")
+    while True:
+        try:
+            '''
+            if(Time_ms() - timeLast_ms > 8e2):
+                print(".")
+                timeLast_ms = Time_ms()
+            '''
+            if(Ser_Recorded.inWaiting() > 0):
+                line_bytes = Ser_Recorded.readline()
+                timeLast_ns = Time_ns()
+                line_str = line_bytes[0:len(line_bytes)-1].decode("utf-8")
+                print(str(timeLast_ns) + " >> " + line_str)
+                writer.writerow([timeLast_ns,line_str])
+            
+        except:
+            print("Keyboard Interrupt")
+            Ser_Recorded.close()
+            break
